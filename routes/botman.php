d132d0ec8fb6991ee7f2e40072c215f35df4b506
Umar Farouq <upharouk@gmail.com>
2018-06-29 07:45:22 +0000

    Removed commented code and add /start function

--- routes/botman.php
+++ routes/botman.php
@@ -4,65 +4,9 @@ use BotMan\BotMan\Middleware\ApiAi;
 
 $botman = resolve('botman');
 
-/*
-$botman->hears('Hi|Hello|Hey', function ($bot) {
-    $bot->reply('Hello there, I am a chatbot that offers few functionality for now');
-    $bot->ask('What is your name?', function ($name, $bot) {
-    	$bot->say('Welcome '.$name->getText());
-    });
-});
-
-
-$botman->hears('How are you?|How you doing?|What\'s up', function ($bot) {
-	$bot->reply('I\'m good');
-	$bot->ask('You?', function ($answer, $bot) {
-		if ($answer == 'Good') {
-			$bot->say('Oh! That\'s good to hear');
-		} elseif ($answer == 'Not good') {
-			$bot->say('Oh! sorry, how about I make you smile');
-		} else {
-			$bot->say('Okay, bright day ahead');
-		}
-	});
-});
-
-
-$botman->hears('Meaning of {dic}?', function ($bot, $dic) {
-	$bot->typesAndWaits(2);
-
-	$url = "http://api.pearson.com/v2/dictionaries/laad3/entries?headword={$dic}&limit=1";
-	$data = file_get_contents($url);
-
-	$lists = json_decode($data);
-
-	$def = $lists->results[0]->senses[0]->definition;
-	$exp = $lists->results[0]->senses[0]->examples[0]->text;
-	$bot->reply('Definition - ' .$def);
-	$bot->reply('Example - ' .$exp);
-});
-
-
-$botman->hears('Google - {google}', function ($bot, $google) {
-	$bot->typesAndWaits(2);
-
-	$url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyClcp2H1eKNSOzDy_uw6DFtKLDv5_rS4-0&cx=017576662512468239146:omuauf_lfve&q={$google}";
-	$data = file_get_contents($url);
-
-	$lists = json_decode($data);
-
-	$title1 = $lists->items[0]->title . "\n";
-	$link1 = $lists->items[0]->link . "\n";
-	$displayLink1 = $lists->items[0]->displayLink . "\n";
-	$snippet1 = $lists->items[0]->snippet . "\n";
-
-	$bot->reply($title1 );
-
-});
-
 $botman->hears('/start', BotManController::class.'@startConversation');
-*/
 
-$dialogflow = ApiAi::create('9794b3192ae84387b8a34b535c1e1205')->listenForAction();
+$dialogflow = ApiAi::create('')->listenForAction();
 
 //Apply global "received" middleware
 
@@ -75,6 +19,10 @@ $botman->hears('.*', function ($bot) {
 	$apiAction = $extras['apiAction'];
 	$apiIntent = $extras['apiIntent'];
 
+	if ($apiReply == '') {
+		$apiReply = 'Not able to connect to dialogflow';
+	}
+	
 	$bot->reply($apiReply);
 
 })->middleware($dialogflow);
